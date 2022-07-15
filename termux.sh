8ac6c696cfe09c68c7ca70def4129af52394a9ef
Giulio <73996381+cangebac@users.noreply.github.com>
2022-07-15 15:18:35 +0200

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,11 +6,8 @@ apt install root-repo
 termux-setup-storage
 apt install nmap
 apt install curl
-apt install git
 apt install python
 apt install perl
 apt install tsu
 apt install vim
-git clone https://github.com/dracula/termux
-cd termux
-mv colors.properties /data/data/com.termux/files/home/.termux
+curl -o $HOME/.termux/colors.properties https://raw.githubusercontent.com/catppuccin/termux/main/colors.properties
