db215351742f310a069d11d520deddf35f66e6dd
Giulio <73996381+cangebac@users.noreply.github.com>
2022-07-15 15:11:33 +0200

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,16 @@
+pkg update
+pkg upgrade
+apt update
+apt upgrade
+apt install root-repo
+termux-setup-storage
+apt install nmap
+apt install curl
+apt install git
+apt install python
+apt install perl
+apt install tsu
+apt install vim
+git clone https://github.com/dracula/termux
+cd termux
+mv colors.properties /data/data/com.termux/files/home/.termux
